def482cd1b11a88da7911a24e075caa9b5344b81
Kenneth Söderlund <kenneth.soderlund@gmail.com>
2018-11-22 11:14:10 +0200

    Add documentation
    Add RAML specification
    Add yarn.lock
    Add invoke task to generate documentation

--- tasks.py
+++ tasks.py
@@ -8,7 +8,8 @@ from pathlib import Path
 from invoke import task
 
 # define projects directories
-app_dir = Path('.')
+APP_DIR = Path('.')
+API_DOCS_DIR = Path(f'{APP_DIR}/docs')
 
 
 @task
@@ -32,7 +33,7 @@ def test(ctx):
 @task
 def check(ctx):
     """Run pycodestyle on the project."""
-    ctx.run('pycodestyle application.py tasks.py ' + str(app_dir))
+    ctx.run('pycodestyle application.py tasks.py ' + str(APP_DIR))
 
 
 @task
@@ -40,10 +41,37 @@ def clean(ctx):
     """Clean any python generated files and folders."""
     for pattern in ['__pycache__', '.pytest_cache',
                     '*.pyc', '*.pyo', '*~', '._*']:
-        for f in app_dir.rglob(pattern):
+        for f in APP_DIR.rglob(pattern):
             if f.is_dir():
                 print('Removing tree {}'.format(f.name))
                 shutil.rmtree(f)
             else:
                 print('Removing file {}'.format(f.name))
                 f.unlink()
+
+
+@task
+def docs(ctx):
+    """Generate the documentation for the API.
+
+    :param ctx: The invoke context.
+    :type ctx: invoke.Context
+    :return: None
+    :rtype: None
+    :raise RuntimeError:
+    """
+    print('Generating documentation...')
+    raml2html_path = Path(f'{APP_DIR.absolute()}/'
+                          f'node_modules/.bin/raml2html')
+
+    if not raml2html_path.exists():
+        raise RuntimeError(
+            'You need to install raml2html, use yarn install in the root.'
+        )
+
+    ctx.run(f'yarn run -s generate '
+            f'{API_DOCS_DIR.absolute()}/api.raml > '
+            f'{API_DOCS_DIR.absolute()}/index.html')
+
+    print(f'Documentation generated to '
+          f'"{API_DOCS_DIR}/index.html"')
